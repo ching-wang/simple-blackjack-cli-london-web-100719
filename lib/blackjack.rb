120f9ac44eb0f919439d18bbb9625ccaeaa12082
Qing Wang <chingwang.qw@gmail.com>
2019-10-07 20:52:34 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -24,13 +24,14 @@ def end_game(num)
  puts  "Sorry, you hit #{num}. Thanks for playing!"
 end
 
-binding.pry
+
 def initial_round
   total = deal_card().to_i + deal_card().to_i
   display_card_total(total)
 end
 
 def hit?()
+  binding.pry
   # code hit? here
 end
 

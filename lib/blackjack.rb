1c8d613eb7c4c8321776793edc1197ea2cc36573
Qing Wang <chingwang.qw@gmail.com>
2019-10-07 20:29:57 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -19,7 +19,7 @@ def get_user_input
 end
 
 def end_game(num)
- "Sorry, you hit #{card_total}. Thanks for playing!"
+ return "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
